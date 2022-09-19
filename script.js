cfab9c6c933b8648c163229fbde97567084fc751
Berresu <90646787+Berresu@users.noreply.github.com>
2022-09-19 16:40:51 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let gun;
+switch(new Date().getDay()){
+    case 0:
+        gun="Pazar";
+        break;
+    case 1:
+        gun="Pazartesi";
+        break;
+    case 2:
+        gun="Salı";
+        break;
+    case 3:
+        gun="Çarşamba";
+        break;
+    case 4:
+        gun="Perşembe";
+        break;
+    case 5:
+        gun="Cuma";
+        break;
+    case 6:
+        gun="Cumartesi";
+        break;                        
+}
+document.getElementById("sonuc").innerHTML="Bugün "+gun;
